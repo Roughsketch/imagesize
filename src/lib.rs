34ccbd09946757c258d8cb10c88df3855f863be8
Maid Dog <maiddogsrl@gmail.com>
2017-04-30 20:47:19 -0500

    Add getting data from blob.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::path::Path;
 use std::io::Read;
-use std::io::{BufRead, BufReader, Seek};
+use std::io::{BufRead, BufReader};
 
 #[cfg(test)]
 mod tests {
@@ -61,6 +61,29 @@ mod tests {
             Err(why) => println!("Error getting dimensions: {:?}", why)
         }
     }
+
+    #[test]
+    fn blob_test() {
+        //  PNG Header with size 123x321
+        let data = vec![0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 
+                        0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 
+                        0x00, 0x00, 0x00, 0x7B, 0x00, 0x00, 0x01, 0x41,
+                        0x08, 0x06, 0x00, 0x00, 0x00, 0x9A, 0x38, 0xC4];
+
+        match get_dimensions_from_blob(data) {
+            Ok(dim) => {
+                assert_eq!(dim.width, 123);
+                assert_eq!(dim.height, 321);
+            }
+            Err(why) => println!("Error getting dimensions: {:?}", why)
+        }
+    }
+
+    #[test]
+    fn blob_too_small_test() {
+        let data = vec![0x89, 0x00, 0x01, 0x02];
+        assert_eq!(get_dimensions_from_blob(data).is_err(), true);
+    }
 }
 
 pub enum ImageType {
@@ -107,9 +130,25 @@ pub fn get_dimensions<P>(path: P) -> ImageResult<Dimensions> where P: AsRef<Path
     }
 }
 
-fn get_bmp_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
+pub fn get_dimensions_from_blob(data: Vec<u8>) -> ImageResult<Dimensions> {
+    let mut reader = BufReader::new(&data[..]);
+
+    let mut header = [0; 1];
+    try!(reader.read_exact(&mut header));
+
+    match header[0] {
+        0xFF => get_jpeg_dimensions(&mut reader),
+        0x89 => get_png_dimensions(&mut reader),
+        b'R' => get_webp_dimensions(&mut reader),
+        b'G' => get_gif_dimensions(&mut reader),
+        b'B' => get_bmp_dimensions(&mut reader),
+        _ => Err(ImageError::NotSupported("Could not decode image.".to_owned()))
+    }
+}
+
+fn get_bmp_dimensions<R: BufRead>(reader: &mut R) -> ImageResult<Dimensions> {
     let mut buffer = [0; 8];
-    try!(reader.seek(std::io::SeekFrom::Start(0x12)));
+    reader.consume(0x11);
     try!(reader.read_exact(&mut buffer));
 
     Ok(Dimensions {
@@ -125,9 +164,9 @@ fn get_bmp_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
     })
 }
 
-fn get_gif_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
+fn get_gif_dimensions<R: BufRead>(reader: &mut R) -> ImageResult<Dimensions> {
     let mut buffer = [0; 4];
-    try!(reader.seek(std::io::SeekFrom::Start(6)));
+    reader.consume(5);
     try!(reader.read_exact(&mut buffer));
 
     Ok(Dimensions {
@@ -136,7 +175,7 @@ fn get_gif_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
     })
 }
 
-fn get_jpeg_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
+fn get_jpeg_dimensions<R: BufRead>(reader: &mut R) -> ImageResult<Dimensions> {
     let mut search = Vec::new();
     let mut buffer = [0; 4];
     let mut page = [0; 1];
@@ -145,7 +184,7 @@ fn get_jpeg_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> 
         let _ = try!(reader.read_until(0xFF, &mut search));
         try!(reader.take(1).read(&mut page));
         if page[0] == 0xC0 {
-            try!(reader.seek(std::io::SeekFrom::Current(3)));
+            reader.consume(3);
             break;
         }
     }
@@ -158,9 +197,9 @@ fn get_jpeg_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> 
     })
 }
 
-fn get_png_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
+fn get_png_dimensions<R: BufRead>(reader: &mut R) -> ImageResult<Dimensions> {
     let mut buffer = [0; 8];
-    try!(reader.seek(std::io::SeekFrom::Start(16)));
+    reader.consume(15);
     try!(reader.read_exact(&mut buffer));
 
     Ok(Dimensions {
@@ -176,9 +215,9 @@ fn get_png_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
     })
 }
 
-fn get_webp_dimensions(reader: &mut BufReader<File>) -> ImageResult<Dimensions> {
+fn get_webp_dimensions<R: BufRead>(reader: &mut R) -> ImageResult<Dimensions> {
     let mut buffer = [0; 6];
-    try!(reader.seek(std::io::SeekFrom::Start(0x18)));
+    reader.consume(0x17);
     try!(reader.read_exact(&mut buffer));
 
     Ok(Dimensions {
