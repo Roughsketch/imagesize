d59267b62013dffc4ba5a4c42e1959cb3678c4b4
Allen <allengnr@gmail.com>
2023-05-05 20:02:14 +0800

    Change logic base on author's comments

--- src/formats/ktx2.rs
+++ src/formats/ktx2.rs
@@ -1,25 +1,15 @@
-use std::io::{self, BufRead, Seek, SeekFrom};
+use std::io::{BufRead, Seek, SeekFrom};
 
-use crate::{ImageResult, ImageSize};
+use crate::{
+    util::{read_u32, Endian},
+    ImageResult, ImageSize,
+};
 
 pub fn size<R: BufRead + Seek>(reader: &mut R) -> ImageResult<ImageSize> {
-    reader.seek(SeekFrom::Start(0))?;
+    reader.seek(SeekFrom::Start(16))?;
 
-    let mut identifier = [0; 12];
-    reader.read_exact(&mut identifier)?;
-    let ktx2_identifier = [
-        0xAB, 0x4B, 0x54, 0x58, 0x20, 0x32, 0x30, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A,
-    ];
-
-    if identifier != ktx2_identifier {
-        return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid KTX2 identifier").into());
-    }
-
-    let mut header = [0; 40];
-    reader.read_exact(&mut header)?;
-
-    let width = u32::from_le_bytes([header[4], header[5], header[6], header[7]]) as usize;
-    let height = u32::from_le_bytes([header[8], header[9], header[10], header[11]]) as usize;
+    let width = read_u32(reader, &Endian::Little)? as usize;
+    let height = read_u32(reader, &Endian::Little)? as usize;
 
     Ok(ImageSize { width, height })
 }
