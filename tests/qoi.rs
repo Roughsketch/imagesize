0391beac4c593f749e151ef60c5280dad31fbc1e
Allen <allengnr@gmail.com>
2023-05-05 20:05:27 +0800

    Remove debug print

--- tests/qoi.rs
+++ tests/qoi.rs
@@ -4,7 +4,6 @@ use imagesize::size;
 #[test]
 fn qoi_test() {
     let dim = size("tests/images/qoi/test.qoi").unwrap();
-    println!("{:?}", dim);
     assert_eq!(dim.width, 800);
     assert_eq!(dim.height, 600);
 }
